6feaad7a9b63aff2363d6f2299c549982336c1d4
DESKTOP-12IM60C\Horhia <georgeo4567@gmail.com>
2019-12-08 00:22:31 +0200

    Added getRecipeById endpoint

--- backend/src/main/java/com/softdight/instantorder/backend/controller/MenuController.java
+++ backend/src/main/java/com/softdight/instantorder/backend/controller/MenuController.java
@@ -1,13 +1,19 @@
 package com.softdight.instantorder.backend.controller;
 
+import com.softdight.instantorder.backend.model.Recipe;
 import com.softdight.instantorder.backend.service.IngredientService;
 import com.softdight.instantorder.backend.service.RecipeService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,6 +37,17 @@ public class MenuController {
     public ResponseEntity<?> findAllRecipes(){
         return new ResponseEntity<>(recipeService.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping("recipe/get-by-id")
+    @ApiOperation(value = "returns a recipe with that id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = Recipe.class)
+    })
+    public ResponseEntity<?> getRecipeById(@ApiParam(example = "PIZZA CLASSIC") @RequestParam String recipeId){
+        return new ResponseEntity<>(recipeService.findById(recipeId), HttpStatus.OK);
+    }
+
+
 
 
 }
--- backend/src/main/java/com/softdight/instantorder/backend/model/RecipeIngredient.java
+++ backend/src/main/java/com/softdight/instantorder/backend/model/RecipeIngredient.java
@@ -1,6 +1,7 @@
 package com.softdight.instantorder.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ public class RecipeIngredient {
 
     @Id
     @Column(name = "RECIPE_ID")
+    @JsonIgnore
     private String recipe;
 
     @Id
